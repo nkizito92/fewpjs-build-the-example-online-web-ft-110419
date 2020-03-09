c6d77067e806e545dd748fd92fbe5fe529aa57df
Kizito Njoku <nkizito57@gmail.com>
2020-03-09 18:26:15 +0000

    Done.

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
+const mod = document.querySelector("#modal").className="hidden";
+let like = document.querySelectorAll(".like");
 
+  like.forEach(h => {
+    h.addEventListener("click", liketoggle );
+  });
 
-
+function liketoggle(e){
+  let likes = e.target.querySelector(".like-glyph");
+    if (likes.innerText === EMPTY_HEART){
+      likes.innerText = FULL_HEART;
+      likes.style.color = "red";
+    }
+    else {
+      likes.innerText = EMPTY_HEART;
+      likes.style.color = "#AAB8C2";
+    }
+    mimicServerCall()
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
