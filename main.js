2d48459d93d6c08191ff16d6bb1f2aefa1dc4ca0
Kizito Njoku <nkizito57@gmail.com>
2020-03-09 18:41:13 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -3,56 +3,25 @@ const EMPTY_HEART = '♡';
 const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
-// const mod = document.querySelector("#modal").className="hidden";
-// let like = document.querySelectorAll(".like");
+const mod = document.querySelector("#modal").className="hidden";
+let like = document.querySelectorAll(".like");
 
-//   like.forEach(h => {
-//     h.addEventListener("click", liketoggle );
-//   });
+  like.forEach(h => {
+    h.addEventListener("click", liketoggle );
+  });
 
-// function liketoggle(e){
-//   mimicServerCall("bogusUrl")
-//   let likes = e.target.querySelector(".like-glyph");
-//     if (likes.innerText === EMPTY_HEART){
-//       likes.innerText = FULL_HEART;
-//       likes.style.color = "red";
-//     }
-//     else {
-//       likes.innerText = EMPTY_HEART;
-//       likes.style.color = "#AAB8C2";
-//     }
-//     mimicServerCall()
-// }
-
-let glyphStates = {
-  "♡": "♥",
-  "♥": "♡"
-};
-
-let colorStates = {
-  "red" : "",
-  "": "red"
-};
-
-let articleHearts = document.querySelectorAll(".like");
-
-function likeCallback(e) {
-  let heart = e.target;
+function liketoggle(e){
   mimicServerCall("bogusUrl")
-   //OR: mimicServerCall("bogusUrl", {forceFailure: true})
-    .then(function(serverMessage){
-       heart.innerText = glyphStates[heart.innerText];
-       heart.style.color = colorStates[heart.style.color];
-    })
-    .catch(function(error) {
-      document.getElementById("modal").className = "";
-      let err = document.getElementById("modal");
-      err.innerText = error.message;
-    });
-}
-
-for (let glyph of articleHearts) {
-  glyph.addEventListener("click", likeCallback);
+  let likes = e.target.querySelector(".like-glyph");
+    if (likes.innerText === EMPTY_HEART){
+      likes.innerText = FULL_HEART;
+      likes.style.color = "red";
+    }
+    else {
+      likes.innerText = EMPTY_HEART;
+      likes.style.color = "#AAB8C2";
+    }
+    mimicServerCall()
 }
 
 //------------------------------------------------------------------------------
